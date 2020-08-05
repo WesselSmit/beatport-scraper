21db2b35881184eecc0a6b4a66bb49482cd2fe41
Wessel Smit <wrsmit00@gmail.com>
2020-08-06 00:00:04 +0200

    create logger module

---
+++ modules/logger.js
@@ -0,0 +1,14 @@
+module.exports = log
+
+
+/**
+ * Special print scraper specific logs 
+ * @param {string} str - String to print
+ */
+
+function log(str) {
+	/* '\x1b[32m' is the print color,
+	   '%s\x1b[0m' is the reset code and ensures the next logs don't inherit the print color. */
+	const color = '\x1b[32m%s\x1b[0m'
+	console.log(color, `[scraper] ${str}`)
+}
--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 const args = process.argv.slice(2)
 const select = require('./modules/selectors')
+const log = require('./modules/logger')
 
 const accURL = args[0]
 
